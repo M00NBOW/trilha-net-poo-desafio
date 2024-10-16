6ec06b2330767707a8ad7beb47c87282d255ace9
Anderson Martins <andersonhenrique1998@hotmail.com>
2024-10-16 15:31:15 -0300

    Desafio concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizar os testes com as classes Nokia e Iphone
+Iphone i7 = new Iphone("123", "7", "1257abc", 128);
+i7.Ligar();
+i7.ReceberLigacao();
+i7.InstalarAplicativo("WhatsApp");
+
+Nokia n3 = new Nokia("523","N5","6985",32);
+n3.Ligar();
+n3.ReceberLigacao();
+n3.InstalarAplicativo("Snake");
+
